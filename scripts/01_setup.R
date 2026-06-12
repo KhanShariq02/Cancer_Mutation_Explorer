1b9e2addefe488f7b4bcda8451b4a19eef7f1d85
KhanShariq02 <shariq20702@gmail.com>
2026-06-13 00:00:24 +0530

    R setup

---
+++ scripts/01_setup.R
@@ -0,0 +1,8 @@
+install.packages("tidyverse")
+install.packages("BiocManager")
+BiocManager::install("maftools")
+
+library(tidyverse)
+library(maftools)
+
+cat("Cancer Mutation Explorer started!\n")
